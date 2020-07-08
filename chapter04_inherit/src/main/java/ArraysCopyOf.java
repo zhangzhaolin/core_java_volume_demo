a63bcad3ff5f4f0b8de1cf2c588f1bd99eeb7fa2
zhangzhaolin <626344962@qq.com>
2020-07-08 16:24:03 +0800

    反射修改完善

--- chapter04_inherit/src/main/java/ArraysCopyOf.java
+++ chapter04_inherit/src/main/java/ArraysCopyOf.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -8,11 +9,18 @@ import java.util.Arrays;
  */
 public class ArraysCopyOf {
     public static void main(String[] args) {
-        goodCopyOf(new Integer[]{1, 2, 3}, 10);
-        goodCopyOf(new Integer[]{1, 2, 3}, 20);
+        System.out.println(Arrays.toString((Integer[]) goodCopyOf(new Integer[]{1, 2, 3}, 10)));
+        System.out.println(Arrays.toString((int[]) goodCopyOf(new int[]{1, 2, 3}, 8)));
     }
 
-    private static <T> T[] goodCopyOf(T[] a, int newLength) {
-        return Arrays.copyOf(a, newLength);
+    private static Object goodCopyOf(Object a, int newLength) {
+        Class<?> aClass = a.getClass();
+        if (!aClass.isArray()) return null;
+        Class<?> componentType = aClass.getComponentType();
+        int length = Array.getLength(a);
+
+        Object newArray = Array.newInstance(componentType, newLength);
+        System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
+        return newArray;
     }
 }
---
+++ chapter04_inherit/src/main/java/MethodInvoke.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author zhangzhaolin
+ * @version 1.0
+ * @description 调用任意方法
+ * @date 2020/7/8 15:58
+ */
+public class MethodInvoke {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method sqrt = Math.class.getMethod("sqrt", double.class);
+        // sqrt.invoke()
+        for (double i = 1; i <= 10; i++) {
+            double result = (double) sqrt.invoke(null, i);
+            System.out.printf("%f %f  \n", i, result);
+        }
+    }
+
+}
