bc317b11f0e73a8361b6e2ea1bea856a6c9890b7
zhangzhaolin <626344962@qq.com>
2020-07-09 18:23:30 +0800

    add:接口

---
+++ chapter05_interface/src/main/java/MySelfInterface.java
@@ -0,0 +1,17 @@
+public interface MySelfInterface {
+
+    /**
+     * 静态方法
+     */
+    public static void staticFunction() {
+
+    }
+
+    /**
+     * 默认方法
+     */
+    public default void defaultFunction() {
+
+    }
+
+}
